5ad73d284b690d7ac9a4e675d40eb2f6e374c518
Gary <garyherd@yahoo.com>
2015-04-05 17:43:25 -0500

    Modify more lesson files

--- sqlb.py
+++ sqlb.py
@@ -1,1 +1,22 @@
-__author__ = 'G'
+# INSERT Command with Error Handler
+
+
+# import the sqlite3 library
+import sqlite3
+
+# create the connection object
+conn = sqlite3.connect("new.db")
+
+# get a cursor object used to execute SQL commands
+cursor = conn.cursor()
+
+try:
+    # insert data
+    cursor.execute("INSERT INTO populations VALUES('New York City', 'NY', 8200000)")
+    cursor.execute("INSERT INTO populations VALUES('San Francisco', 'CA', 800000)")
+
+    # commit the changes
+    conn.commit()
+except sqlite3.OperationalError as e:
+    print "Oops! Something went wroing. Try again..."
+    print e.args
